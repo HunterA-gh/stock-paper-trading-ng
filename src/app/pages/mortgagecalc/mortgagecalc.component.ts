c21fd49bcc15a1e0e9305a2d85ab93e06b40605c
Hunter <79298881+IsThisUsernameTaken1@users.noreply.github.com>
2021-07-07 12:48:17 -0400

    finished all charts, need to design

--- src/app/pages/mortgagecalc/mortgagecalc.component.ts
+++ src/app/pages/mortgagecalc/mortgagecalc.component.ts
@@ -9,27 +9,11 @@ export class MortgagecalcComponent implements OnInit {
 
   constructor() {
   }
+;
 
   principleAmount = 0;
   termLength = 0;
   interestRate = 0;
-
-
-  monthlyPaymentFormula(): number {
-    return (this.principleAmount * ((this.interestRate * 0.01 / 12) * Math.pow(((this.interestRate * 0.01 / 12) + 1), (this.termLength * 12)))) / ((Math.pow((1 + (this.interestRate * 0.01 / 12)), (this.termLength * 12)) - 1));
-  };
-
-  totalInterest(): number {
-    return (this.monthlyPaymentFormula() * (this.termLength * 12)) - this.principleAmount;
-  }
-
-  totalCost(): number {
-    return (this.monthlyPaymentFormula() * (this.termLength * 12))
-  }
-
-  monthAmount(): number {
-    return this.termLength * 12;
-  }
 
   chartType = 'pie';
   chartLabels = ['Principle', 'Interest'];
@@ -45,25 +29,12 @@ export class MortgagecalcComponent implements OnInit {
       }
     }
   }
-  visualizeData(): void {
-    this.chartData = [this.principleAmount, this.totalInterest()];
-    this.chartLabels2 = [];
-    for(let i = 0; i <= this.termLength; i++){
-      this.chartLabels2.push('Year ' + i);
-    }
-    let paymentLeft = this.totalCost();
-    this.chartData2[0].data = [];
-    for(let i = 0; i <= this.termLength; i++){
-      this.chartData2[0].data.push(paymentLeft)
-      paymentLeft -= 15000;
-    }
-  }
 
 
   chartType2 = 'line';
   chartData2 = [{
     data: [],
-    label: 'Mortgage Amortization',
+    label: ['Payment Left'],
     fill: true
   }];
   chartLabels2 = [];
@@ -107,6 +78,139 @@ export class MortgagecalcComponent implements OnInit {
     }
   }
 
+  chartType3 = 'line';
+  chartData3 = [{
+    data: [],
+    label: ['Towards Interest'],
+    fill: true
+  }];
+  chartLabels3 = [];
+  chartColors3 = [{
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    borderColor: 'rgb(255,77,77)'
+  }];
+  chartOptions3 = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 10000
+        }
+      }]
+    },
+    annotation: {
+      drawTime: 'beforeDatasetsDraw',
+      annotations: [{
+        type: 'box',
+        id: 'a-box-1',
+        yScaleID: 'y-axis-0',
+        yMin: 0,
+        yMax: 1,
+        backgroundColor: '#4cf03b'
+      }, {
+        type: 'box',
+        id: 'a-box-2',
+        yScaleID: 'y-axis-0',
+        yMin: 1,
+        yMax: 2.7,
+        backgroundColor: '#fefe32'
+      }, {
+        type: 'box',
+        id: 'a-box-3',
+        yScaleID: 'y-axis-0',
+        yMin: 2.7,
+        yMax: 5,
+        backgroundColor: '#fe3232'
+      }]
+    }
+  }
+
+  chartType4 = 'line';
+  chartData4 = [{
+    data: [],
+    label: ['Towards Balance'],
+    fill: true
+  }];
+  chartLabels4 = [];
+  chartColors4 = [{
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    borderColor: 'rgb(26,114,2)'
+  }];
+  chartOptions4 = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 10000
+        }
+      }]
+    },
+    annotation: {
+      drawTime: 'beforeDatasetsDraw',
+      annotations: [{
+        type: 'box',
+        id: 'a-box-1',
+        yScaleID: 'y-axis-0',
+        yMin: 0,
+        yMax: 1,
+        backgroundColor: '#4cf03b'
+      }, {
+        type: 'box',
+        id: 'a-box-2',
+        yScaleID: 'y-axis-0',
+        yMin: 1,
+        yMax: 2.7,
+        backgroundColor: '#fefe32'
+      }, {
+        type: 'box',
+        id: 'a-box-3',
+        yScaleID: 'y-axis-0',
+        yMin: 2.7,
+        yMax: 5,
+        backgroundColor: '#fe3232'
+      }]
+    }
+  }
+
+
+  monthlyPayment(): number {
+    return (this.principleAmount * ((this.interestRate * 0.01 / 12) * Math.pow(((this.interestRate * 0.01 / 12) + 1), (this.termLength * 12)))) / ((Math.pow((1 + (this.interestRate * 0.01 / 12)), (this.termLength * 12)) - 1));
+  }
+  totalInterest(): number {
+    return (this.monthlyPayment() * (this.termLength * 12)) - this.principleAmount;
+  }
+
+  totalCost(): number {
+    return (this.monthlyPayment() * (this.termLength * 12))
+  }
+
+  monthAmount(): number {
+    return this.termLength * 12;
+  }
+  visualizeData(): void {
+    this.chartData = [this.principleAmount, this.totalInterest()];
+    this.chartLabels2 = [];
+    this.chartLabels3 = [];
+    this.chartLabels4 = [];
+
+    this.chartData2[0].data = [];
+    this.chartData3[0].data = [];
+    this.chartData4[0].data = [];
+
+    let paymentLeft = this.totalCost();
+
+    for(let i = 0; i <= this.termLength*12; i++){
+      this.chartLabels2.push('Month ' + i);
+      this.chartLabels3.push('Month ' + i);
+      this.chartLabels4.push('Month ' + i);
+
+
+      this.chartData2[0].data.push(paymentLeft);
+      this.chartData3[0].data.push( ( paymentLeft * ( (this.interestRate*0.01)/12) ) );
+      this.chartData4[0].data.push( (this.monthlyPayment() - (paymentLeft * ( (this.interestRate*0.01)/12)  )));
+      paymentLeft -= (this.monthlyPayment());
+    }
+  }
 
   ngOnInit(): void {
   }
